eda7c28b2a230acdd919177da5538d7f49d655a2
Jhefferson Araujo <jhefferson.araujo@gmail.com>
2024-05-27 17:56:53 -0300

    sections added

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function changeSection(section) {
     document.querySelector('.active').classList.remove('active');
     document.querySelector("." + section).classList.add('active');
+    document.querySelector('.main').classList.remove('main');
+    document.getElementById(section).classList.add('main');
 }
